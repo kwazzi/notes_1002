b857f6216f5e7aeeb4ec2fa7c7665d31a9514229
stu41471 <ashlyn.kwasnica71@stu.bryanisd.org>
2019-10-02 09:25:15 -0500

    i calculated the area and printed it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    int height = 2;
 	    int width = 7;
-
+        int area = height*width;
+        System.out.println(area);
     }
 }
